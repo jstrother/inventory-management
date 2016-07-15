d4b45b69c0d27ba6bf8d0d2cc30437f453f686cf
Jim Strother <jim.strother@gmail.com>
2016-07-14 17:18:57 -0700

    added socket.io into the mix

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import {static} from 'express';
+import http from 'http';
+import socket_io from 'socket.io'
 import bodyParser from 'body-parser';
 
 const jsonParser = bodyParser.json();
@@ -9,6 +11,15 @@ const Inventory = function() {
 };
 
 const inventory = new Inventory();
+
+const server = http.Server(app);
+const io = socket_io(server);
+
+io.on('connection', function(socket) {
+	socket.on('', function() {
+
+	});
+});
 
 const app = express();
 app.use('/inventory', static('public'));
