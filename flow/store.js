627ae8c5ca30b810c4730ed9c2e68d38fea3717a
Jim Strother <jim.strother@gmail.com>
2016-07-15 17:32:27 -0700

    adding in redux-socket.io for info flow!

--- flow/store.js
+++ flow/store.js
@@ -1,8 +1,23 @@
 import {applyMiddleware, createStore} from 'redux';
+import createSocketIoMiddleware from 'redux-socket.io';
+import io from 'socket.io-client';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import reducers from './reducers.js';
+
+const socket = io('http://localhost:8080');
+
+const socketIoMiddleware = createSocketIoMiddleware(socket, "server/");
+// do i need the following or do i use reducers imported above?
+function reducer(state = {}, action) {
+	switch(action.type) {
+		case 'message':
+			return Object.assign({}, {message: action.data});
+		default:
+			return state;
+	}
+}
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
