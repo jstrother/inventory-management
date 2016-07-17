c1542b85c9a109778a28436a1e22a2ddb37fdad5
Jim Strother <jim.strother@gmail.com>
2016-07-17 09:06:15 -0700

    added CRUD functions to mongoose snippets

--- snippets-mongoose/snippets.js
+++ snippets-mongoose/snippets.js
@@ -14,3 +14,60 @@ mongoose.connection.once('open', function() {
 	});
 	let Snippet = mongoose.model('Snippet', snippetSchema);
 });
+
+// do i need these CRUD functions?
+let create = (name, content) => {
+	let snippet = {
+		type,
+		lot,
+		expiration,
+		country,
+		quantity
+	};
+	Snippet.create(snippet, function(err, snippet) {
+		if (err || !snippet) {
+			console.error('Could not create snippet', name);
+			mongoose.disconnect();
+			return;
+		}
+		console.log('Created snippet', snippet.name);
+		mongoose.disconnect();
+	});
+};
+
+let read = (name) => {
+	Snippet.findOne({name}, function(err, snippet) {
+		if (err || !snippet) {
+			console.error('Could not read snippet', name);
+			mongoose.disconnect();
+			return;
+		}
+		console.log('Read snippet', snippet.name);
+		console.log(snippet.content);
+		mongoose.disconnect();
+	});
+};
+
+let update = (name, content) => {
+	Snippet.findOneAndUpdate({name}, {content}, function(err, snippet) {
+		if (err || !snippet) {
+			console.error('Could not update snippet', name);
+			mongoose.disconnect();
+			return;
+		}
+		console.log('Updated snippet', snippet.name);
+		mongoose.disconnect();
+	});
+};
+
+let del = (name, content) => {
+	Snippet.findOneAndRemove({name}, function(err, snippet) {
+		if (err || !snippet) {
+			console.error('Could not delete snippet', name);
+			mongoose.disconnect();
+			return;
+		}
+		console.log('Deleted snippet', snippet.name);
+		mongoose.disconnect();
+	});
+};
