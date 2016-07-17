293520b6b0bfefc1895a3b156c55e36aa3eb4920
Jim Strother <jim.strother@gmail.com>
2016-07-17 09:35:18 -0700

    added a mongo data file for inventory

--- flow/actions.js
+++ flow/actions.js
@@ -1,5 +1,43 @@
 // called into reducers.js
 
-// actions to create:
-// 1. createPallet
-// 2. setPalletLocation
+const CREATE_PALLET = 'CREATE_PALLET';
+const createPallet = () => {
+	return {
+		type: CREATE_PALLET
+	};
+};
+
+const SET_PALLET_LOCATION = 'SET_PALLET_LOCATION';
+const setPalletLocation = (pallet, location) => {
+	return {
+		type: SET_PALLET_LOCATION
+	};
+};
+
+const FETCH_INVENTORY_SUCCESS = 'FETCH_INVENTORY_SUCCESS';
+const fetchInventorySuccess = (inventory) => {
+	return {
+		type: FETCH_INVENTORY_SUCCESS,
+		inventory
+	};
+};
+
+const FETCH_INVENTORY_ERROR = 'FETCH_INVENTORY_ERROR';
+const fetchInventoryError = (error) => {
+	return {
+		type: FETCH_INVENTORY_ERROR,
+		error
+	};
+};
+
+const fetchInventory = (inventory) => {
+	return (dispatch) => inventoryFetcher(inventory);
+};
+
+exports.CREATE_PALLET = CREATE_PALLET;
+exports.createPallet = createPallet;
+exports.SET_PALLET_LOCATION = SET_PALLET_LOCATION;
+exports.setPalletLocation = setPalletLocation;
+exports.FETCH_INVENTORY_SUCCESS = FETCH_INVENTORY_SUCCESS;
+exports.FETCH_INVENTORY_ERROR = FETCH_INVENTORY_ERROR;
+exports.fetchInventory = fetchInventory;
--- flow/reducers.js
+++ flow/reducers.js
@@ -2,6 +2,7 @@
 
 import {combineReducers} from 'redux';
 import actions from './actions.js';
+import {fetchInventory} from './actions.js';
 
 const reducers = () => {
 // create several individual reducers and then combine them here
