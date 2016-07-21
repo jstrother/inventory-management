85999ee23b94727224c39149cad391a8b4737ea8
Jim Strother <jim.strother@gmail.com>
2016-07-20 19:04:36 -0700

    added small note to locationSetter.js

--- locationSetter.js
+++ locationSetter.js
@@ -1,26 +1,3 @@
-// const locationSetter = (number, modulo) => {
-// 	let location = [];
-// 	for (i = 0; i <= number; i++) {
-// 		if (i % modulo === 0) {
-// 			for (j = 0; j <= number; j++) {
-// 				if (j <= modulo) {
-// 					location.push(`A${j}`);
-// 				}
-// 				else if (j > modulo * 2) {
-// 					location.push(`C${j - (modulo * 2)}`);
-// 				}
-// 				else {
-// 					location.push(`B${j - modulo}`);
-// 				}
-// 			}
-// 		}
-// 		return location; 
-// 	}
-// };
-
-// length of row equals modulo
-// a, b, or c prepended to 1 - modulo
-
 let locationSetter = (number, modulo) => {
 	let rows = [];
 	let numRows = (number / modulo);  //should be 3 for our example
