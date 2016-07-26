fa6c6ae3e368fcfd3c88e792e37b532c7422a3b3
Jim Strother <jim.strother@gmail.com>
2016-07-25 20:57:33 -0700

    starting on the last of react components

--- components/inventory.jsx
+++ components/inventory.jsx
@@ -3,13 +3,43 @@
 import {createClass} from 'react';
 import {connect} from 'react-redux';
 
-import Type from './type.jsx';
+// Put the following into a switch statement to display grand totals of each
+// Cherry Almond
+// Sea Salt
+// DSA
+// MSA
+// Chili
+// Toasted Coconut
+// 85%
+// 70%
+// 45%
+// Orange
+// Mint
+// Raspberry
+// Ginger
+// BRQC
+// Salted Toffee
+// KC Banana
+// KC Almond
+// KC Raspberry
+// Coconut Curry
+// Bread
+// Fig Fennel
+// Coffee
+// Hazelnut Crunch
+// Chai
+// Almond Butter Cups
+// DPBC
+// MPBC
+// CB - Classic
+// CB - Salted Almond
+// CB - Mint Milk
 
 const Inventory = createClass({
 	render: function() {
 		return (
 			<div className='inventory'>
-				<Type />
+				
 			</div>
 		);
 	}
